a6fc85aae71539d95e47070e5553de103cc0d40c
dyukoffv4 <net998art@gmail.com>
2023-01-22 00:05:16 +0300

    Paint environment added for CW

--- get-opt/src/defines.hpp
+++
@@ -1,8 +0,0 @@
-#pragma once
-
-#include <string>
-#include <vector>
-
-
-typedef std::string Arg;
-typedef std::vector<Arg> Args;
--- get-opt/src/main.cpp
+++ get-opt/src/main.cpp
@@ -1,134 +1,49 @@
-#define _USE_MATH_DEFINES
-#include <cmath>
-#include "terminal.hpp"
-#include "utils.hpp"
+#include "lib/terminal.hpp"
+#include "utils/utils.hpp"
+#include "paint.hpp"
 
 
-class RootLtnr : public Listener {
-public:
-	RootLtnr(Terminal* term) : Listener(term) {}
+// class CenterLtnr : public Listener {
+// public:
+// 	CenterLtnr(Terminal* term) : Listener(term) {}
 
-	virtual Listener* getCopy() {
-		return new RootLtnr(term);
-	}
+// 	virtual Listener* getCopy() {
+// 		return new CenterLtnr(term);
+// 	}
 
-	virtual void execute(Args opts) override {
-		std::cout << "> Root arguments: ";
-		for (auto& i : opts) std::cout << "\"" + i + "\"; ";
-		std::cout << "\n";
-	}
-};
-
-class CenterLtnr : public Listener {
-public:
-	CenterLtnr(Terminal* term) : Listener(term) {}
-
-	virtual Listener* getCopy() {
-		return new CenterLtnr(term);
-	}
-
-	virtual void execute(Args opts) override {
-		if (opts.size() != 2) throw std::invalid_argument("CenterLntr.execute: Only two arguments expected after \"c/center\" key!");
-		try {
-			term->Data("center_x") = opts[0];
-			term->Data("center_y") = opts[1];
-		}
-		catch (std::domain_error exp) {
-			throw std::invalid_argument(Arg("CenterLntr.execute->") + exp.what());
-		}
-	}
-};
-
-class PointsLtnr : public Listener {
-public:
-	PointsLtnr(Terminal* term) : Listener(term) {}
-
-	virtual Listener* getCopy() {
-		return new PointsLtnr(term);
-	}
-
-	virtual void execute(Args opts) override {
-		if (opts.size() != 1) throw std::invalid_argument("PointsLntr.execute: Only one argument expected after \"p/points\" key!");
-		else {
-			try {
-				term->Data("points") = opts[0];
-			}
-			catch (std::exception exp) {
-				throw std::invalid_argument(Arg("PointsLntr.execute->") + exp.what());
-			}
-		}
-	}
-};
-
-class RadiusLtnr : public Listener {
-public:
-	RadiusLtnr(Terminal* term) : Listener(term) {}
-
-	virtual Listener* getCopy() {
-		return new RadiusLtnr(term);
-	}
-
-	virtual void execute(Args opts) override {
-		if (opts.size() != 1) throw std::invalid_argument("RadiusLntr.execute: Only one argument expected after \"r/radius\" key!");
-		else {
-			try {
-				term->Data("radius") = opts[0];
-			}
-			catch (std::exception exp) {
-				throw std::invalid_argument(Arg("RadiusLntr.execute->") + exp.what());
-			}
-		}
-	}
-};
-
-class CircleLtnr : public Listener {
-public:
-	CircleLtnr(Terminal* term) : Listener(term) {}
-
-	virtual Listener* getCopy() {
-		return new CircleLtnr(term);
-	}
-
-	virtual void execute(Args opts) override {
-		if (!opts.empty()) throw std::invalid_argument("CircleLntr.execute: Only keys expected after \"-circle\" key!");
-		else {
-			int center_x(str_num(term->Data("center_x"))), points(str_num(term->Data("points")));
-			int center_y(str_num(term->Data("center_y"))), radius(str_num(term->Data("radius")));
-			std::string xml;
-			xml += "> Circle XML: M" + num_str(center_x + radius) + " " + num_str(center_y) + " ";
-			for (int i = 1; i < points; i++) {
-				xml += "L";
-				xml += num_str(std::cos(i / double(points) * M_PI * 2) * radius + center_x) + " ";
-				xml += num_str(std::sin(i / double(points) * M_PI * 2) * radius + center_y) + " ";
-			}
-			xml += "Z\n";
-			std::cout << xml;
-		}
-	}
-};
+// 	virtual void execute(Args opts) override {
+// 		if (opts.size() != 2) throw std::invalid_argument("CenterLntr.execute: Only two arguments expected after \"c/center\" key!");
+// 		try {
+// 			term->Data("center_x") = opts[0];
+// 			term->Data("center_y") = opts[1];
+// 		}
+// 		catch (std::domain_error exp) {
+// 			throw std::invalid_argument(Arg("CenterLntr.execute->") + exp.what());
+// 		}
+// 	}
+// };
 
 // main
 
 int main(int argc, char* argv[]) {
-	Args data;
-	for (int i = 0; i < argc; i++) data.push_back(argv[i]);
+	// Args data;
+	// for (int i = 0; i < argc; i++) data.push_back(argv[i]);
 
-	try {
-		Terminal terminal;
-		terminal.Data("points") = "10";
-		terminal.Data("radius") = "1";
-		terminal.Data("center_x") = "0";
-		terminal.Data("center_y") = "0";
-		terminal.setRoot(new RootLtnr(&terminal));
-		terminal.setKey(Key("center"), new CenterLtnr(&terminal));
-		terminal.setKey(Key("points"), new PointsLtnr(&terminal));
-		terminal.setKey(Key("radius"), new RadiusLtnr(&terminal));
-		terminal.setKey(Key("xmlcrl"), new CircleLtnr(&terminal));
-		terminal.execute(data);
-	}
-	catch (std::invalid_argument e) {
-		std::cout << e.what() << "\n";
-	}
+	// try {
+	// 	Terminal terminal;
+	// 	// terminal.Data("points") = "10";
+	// 	// terminal.setKey(Key("center"), new CenterLtnr(&terminal));
+	// 	terminal.execute(data);
+	// }
+	// catch (std::invalid_argument e) {
+	// 	std::cout << e.what() << "\n";
+	// }
+
+	Paint::Image image1, image2;
+
+	std::cout << image1.load("pict/simpsonsvr.bmp") << std::endl;
+	image2 = Paint::set_component(Paint::Component{'g', 0}, image1);
+	std::cout << image2.save("pict/test.bmp") << std::endl;
 
 	return 0;
 }
