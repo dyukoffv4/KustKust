74fed6ff693483386e3bc5790df8f982b44d399d
dyukoffv4 <net998art@gmail.com>
2023-01-22 16:06:50 +0300

    Key refactor (short key must to be fixed); Listeners work principles are changed

---
+++ get-opt/src/example/main.cpp
@@ -0,0 +1,105 @@
+#include "../lib/terminal.hpp"
+#include "utils.hpp"
+#include "paint.hpp"
+
+
+namespace Listeners {
+	std::vector<Paint::Image> images(1);
+	std::string save_path;
+
+	void set_load_image_path(Args opts) {
+		if (opts.size() != 2) throw std::invalid_argument("Listeners::set_load_image_path: No working file specified!");
+		try {
+			images[0].load(opts[1]);
+			save_path = opts[1];
+			int pos = opts[1].find(".bmp");
+			if (pos) save_path = opts[1].substr(0, pos) + "_0";
+		}
+		catch (std::domain_error exp) {
+			throw std::invalid_argument(Arg("Listeners::set_load_image_path->") + exp.what());
+		}
+	}
+
+	void set_save_image_path(Args opts) {
+		if (opts.size() != 1) throw std::invalid_argument("Listeners::set_save_image_path: Only one arguments expected after \"o/out\" key!");
+		try {
+			save_path = opts[0];
+			int pos = opts[0].find(".bmp");
+			if (pos) save_path = opts[0].substr(0, pos);
+		}
+		catch (std::domain_error exp) {
+			throw std::invalid_argument(Arg("Listeners::set_save_image_path->") + exp.what());
+		}
+	}
+
+	void set_component(Args opts) {
+		if (opts.size() != 2) throw std::invalid_argument("Listeners::set_component: Only two arguments expected after \"c/color\" key!");
+		try {
+			for (auto& i : images)
+				i = Paint::set_component(Paint::Component{opts[0][0], str_int(opts[1])}, i);
+		}
+		catch (std::domain_error exp) {
+			throw std::invalid_argument(Arg("Listeners::set_component->") + exp.what());
+		}
+	}
+
+	void put_circle(Args opts) {
+		if (opts.size() != 3) throw std::invalid_argument("Listeners::put_cirlce: Only three arguments expected after \"c/circle\" key!");
+		try {
+			for (auto& i : images)
+				i = Paint::put_circle(Paint::Circle{str_int(opts[0]), str_int(opts[1]), str_int(opts[2])}, i);
+		}
+		catch (std::domain_error exp) {
+			throw std::invalid_argument(Arg("Listeners::execute->") + exp.what());
+		}
+	}
+
+	void put_rectangle(Args opts) {
+		if (opts.size() != 6) throw std::invalid_argument("Listeners::put_rectangle: Only six arguments expected after \"s/square\" key!");
+		try {
+			for (auto& i : images)
+				i = Paint::put_rectangle(Paint::Rectangle{str_int(opts[0]), str_int(opts[1]), str_int(opts[2]), str_int(opts[3]), str_int(opts[4]), str_int(opts[5])}, i);
+		}
+		catch (std::domain_error exp) {
+			throw std::invalid_argument(Arg("Listeners::execute->") + exp.what());
+		}
+	}
+
+	void cut_the_crap(Args opts) {
+		if (opts.size() != 2) throw std::invalid_argument("Listeners::set_component: Only two arguments expected after \"s/slice\" key!");
+		try {
+			for (auto& i : Paint::cut_the_crap(Paint::CutLines{str_int(opts[0]), str_int(opts[1])}, images[0])) images.push_back(i);
+			images.erase(images.begin());
+		}
+		catch (std::domain_error exp) {
+			throw std::invalid_argument(Arg("Listeners::execute->") + exp.what());
+		}
+	}
+}
+
+// main
+
+int main(int argc, char* argv[]) {
+	Args data;
+	for (int i = 0; i < argc; i++) data.push_back(argv[i]);
+
+	try {
+		Terminal terminal;
+		terminal.setRoot(Listeners::set_load_image_path);
+		terminal.setKey(Key("out"), Listeners::set_save_image_path);
+		terminal.setKey(Key("color"), Listeners::set_component);
+		terminal.setKey(Key("circle"), Listeners::put_circle);
+		terminal.setKey(Key("slice"), Listeners::cut_the_crap);
+		terminal.setKey(Key("square"), Listeners::put_rectangle);
+		terminal.execute(data);
+
+		if (Listeners::images.size() == 1) std::cout << Listeners::images[0].save(Listeners::save_path + ".bmp") << std::endl;
+		else for (int i = 0; i < Listeners::images.size(); i++)
+			std::cout << Listeners::images[i].save(Listeners::save_path + dub_str(i) + ".bmp") << std::endl;
+	}
+	catch (std::invalid_argument e) {
+		std::cout << e.what() << "\n";
+	}
+
+	return 0;
+}
---
+++ get-opt/src/example/paint.hpp
@@ -0,0 +1,100 @@
+#pragma once
+
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
+
+
+namespace Paint {
+#pragma pack(push, 1)
+
+    struct BitmapFileHeader {
+        unsigned short signature;
+        unsigned int filesize;
+        unsigned short reserved1;
+        unsigned short reserved2;
+        unsigned int pixelArrOffset;
+    };
+
+    struct BitmapInfoHeader {
+        unsigned int headerSize;
+        unsigned int width;
+        unsigned int height;
+        unsigned short planes;
+        unsigned short bitsPerPixel;
+        unsigned int compression;
+        unsigned int imageSize;
+        unsigned int xPixelsPerMeter;
+        unsigned int yPixelsPerMeter;
+        unsigned int colorsInColorTable;
+        unsigned int importantColorCount;
+    };
+
+    struct BGR {
+        unsigned char b;
+        unsigned char g;
+        unsigned char r;
+    };
+
+    struct Rectangle {
+        int x1;
+        int y1;
+        int x2;
+        int y2;
+        int x_to;
+        int y_to;
+    };
+
+    struct CutLines {
+        int x_num;
+        int y_num;
+    };
+
+    struct Component {
+        char index;
+        int value;
+    };
+
+    struct Circle {
+        int x0;
+        int y0;
+        int rad_b;
+        int width;
+        BGR l_color;
+        int f_flag;
+        BGR f_color;
+    };
+
+#pragma pack(pop)
+
+    class Image {
+    public:
+        BitmapFileHeader BFH;
+        BitmapInfoHeader BIH;
+        BGR **BM;
+
+        Image();
+        Image(const Image& image);
+        ~Image();
+
+        Image& operator=(const Image& image);
+
+        bool load(std::string path);
+        bool save(std::string path);
+    };
+
+    std::vector<Image> cut_the_crap(CutLines lines, const Image& image);
+
+    Image set_component(Component color, const Image& image);
+
+    Image put_circle(Circle circle, const Image& image);
+
+    Image put_rectangle(Rectangle rectangle, const Image& image);
+
+    std::ostream &operator<<(std::ostream &out, const BitmapFileHeader &bfh);
+
+    std::ostream &operator<<(std::ostream &out, const BitmapInfoHeader &bih);
+
+    std::ostream &operator<<(std::ostream &out, const BGR &bgr);
+}
