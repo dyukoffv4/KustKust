971cbb1ef8559b05428bc0bfb4a3424269ecae50
dyukoffv4 <net998art@gmail.com>
2023-10-25 22:07:14 +0300

    little formating

--- Studies/PA/3/src/serial.cpp
+++ Studies/PA/3/src/serial.cpp
@@ -3,40 +3,38 @@
 #include <vector>
 #include <omp.h>
 
-
 // Generation functions
 
 typedef std::vector<double> vector;
 typedef std::vector<vector> matrix;
 
-matrix Matrix(vector x) {
+matrix matrixSLAE(vector x) {
     matrix m(x.size(), vector(x.size() + 1, 0));
     int s = x.size();
     double b = 0;
-    
+
     for (int i = 0; i < s; i++) {
         m[i][i] += 1;
         for (int j = 0; j < s; j++) b += (m[i][j] += i + j) * x[j];
         m[i][s] += b;
         b = 0;
     }
-    
+
     return m;
 }
 
-std::ostream& operator<<(std::ostream& out, const matrix& m) {
-    for (auto& i : m) {
-        for (auto& j : i) out << std::setw(6) << j << "  ";
+std::ostream &operator<<(std::ostream &out, const matrix &m) {
+    for (auto &i : m) {
+        for (auto &j : i) out << std::setw(6) << j << "  ";
         out << "\n";
     }
     return out;
 }
 
-std::ostream& operator<<(std::ostream& out, const vector& v) {
-    for (auto& i : v) out << std::setw(6) << i << "  ";
+std::ostream &operator<<(std::ostream &out, const vector &v) {
+    for (auto &i : v) out << std::setw(6) << i << "  ";
     return out << "\n";
 }
-
 
 // Algorithm functions
 
@@ -44,27 +42,27 @@ double determinant(const matrix m, int y, std::vector<int> xs) {
     if (xs.size() == 2) return m[y][xs[0]] * m[y + 1][xs[1]] - m[y][xs[1]] * m[y + 1][xs[0]];
     double result = 0.0;
     std::vector<int> nxs;
-    
+
     for (int i = 0; i < xs.size(); i += 2) {
         nxs = xs;
         nxs.erase(nxs.begin() + i);
         result += m[y][xs[i]] * determinant(m, y + 1, nxs);
     }
-    
+
     for (int i = 1; i < xs.size(); i += 2) {
         nxs = xs;
         nxs.erase(nxs.begin() + i);
         result -= m[y][xs[i]] * determinant(m, y + 1, nxs);
     }
-    
+
     return result;
 }
 
-void do_slae_with_kramer(const matrix m, vector& result) {
+void kramerSLAE(const matrix m, vector &result) {
     std::vector<int> xs(m.size());
     for (int i = 0; i < m.size(); i++) xs[i] = i;
     double det = determinant(m, 0, xs);
-    
+
     for (int i = 0; i < m.size(); i++) {
         xs[i] = m.size();
         result[i] = determinant(m, 0, xs) / det;
@@ -72,17 +70,15 @@ void do_slae_with_kramer(const matrix m, vector& result) {
     }
 }
 
-
 // Main
 
 int main() {
     vector ex{0, 1, 2, 3}, x(4);
-    matrix m = Matrix(ex);
-    
+    matrix m = matrixSLAE(ex);
+
     std::cout << m << ex;
-    do_slae_with_kramer(m, x);
+    kramerSLAE(m, x);
     std::cout << '\n' << m << x;
-    
+
     return 0;
 }
-
