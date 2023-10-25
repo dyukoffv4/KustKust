f5c8d0a3f24915d6a3e5c3ad5f30b355789ac369
dyukoffv4 <net998art@gmail.com>
2023-10-25 16:17:16 +0300

    upgrade with  STL

--- Studies/PA/3/src/serial.cpp
+++ Studies/PA/3/src/serial.cpp
@@ -1,71 +1,73 @@
 #include <iostream>
+#include <iomanip>
 #include <vector>
 #include <omp.h>
 
 
 // Generation functions
 
-double** new_slae_matrix(int v_num) {
-    double** matrix = new double*[v_num];
-    for (int i = 0; i < v_num; i++) {
-        matrix[i] = new double[v_num + 1];
-        for (int j = 0; j < v_num; j++) matrix[i][j] = 1;
+typedef std::vector<double> vector;
+typedef std::vector<vector> matrix;
+
+matrix Matrix(vector x) {
+    matrix m(x.size(), vector(x.size() + 1, 0));
+    int s = x.size();
+    double b = 0;
+    
+    for (int i = 0; i < s; i++) {
+        m[i][i] += 1;
+        for (int j = 0; j < s; j++) b += (m[i][j] += i + j) * x[j];
+        m[i][s] += b;
+        b = 0;
     }
-    return matrix;
+    
+    return m;
 }
 
-double** new_slae_matrix(int v_num, const double* xs) {
-    double** matrix = new double*[v_num], result;
-    for (int i = 0; i < v_num; i++) {
-        matrix[i] = new double[v_num + 1];
-        result = 0.0;
-        for (int j = 0; j < v_num; j++) result += (matrix[i][j] = i + j) * xs[j];
-        matrix[i][i] += 1;
-        matrix[i][v_num] = result + xs[i];
+std::ostream& operator<<(std::ostream& out, const matrix& m) {
+    for (auto& i : m) {
+        for (auto& j : i) out << std::setw(6) << j << "  ";
+        out << "\n";
     }
-    return matrix;
+    return out;
 }
 
-void delete_slae_matrix(double** matrix, int v_num) {
-    for (int i = 0; i < v_num; i++) delete[] matrix[i];
-    delete[] matrix;
-}
-
-void print_matrix(const double* const* matrix, int h, int w) {
-    for (int i = 0; i < h; i++) {
-        for (int j = 0; j < w; j++) std::printf("%6.2f  ", matrix[i][j]);
-        std::printf("\n");
-    }
+std::ostream& operator<<(std::ostream& out, const vector& v) {
+    for (auto& i : v) out << std::setw(6) << i << "  ";
+    return out << "\n";
 }
 
 
 // Algorithm functions
 
-double determinant(const double* const* matrix, int y, std::vector<int> xs) {
-    if (xs.size() == 2) return matrix[y][xs[0]] * matrix[y + 1][xs[1]] - matrix[y][xs[1]] * matrix[y + 1][xs[0]];
+double determinant(const matrix m, int y, std::vector<int> xs) {
+    if (xs.size() == 2) return m[y][xs[0]] * m[y + 1][xs[1]] - m[y][xs[1]] * m[y + 1][xs[0]];
     double result = 0.0;
     std::vector<int> nxs;
+    
     for (int i = 0; i < xs.size(); i += 2) {
         nxs = xs;
         nxs.erase(nxs.begin() + i);
-        result += matrix[y][xs[i]] * determinant(matrix, y + 1, nxs);
+        result += m[y][xs[i]] * determinant(m, y + 1, nxs);
     }
+    
     for (int i = 1; i < xs.size(); i += 2) {
         nxs = xs;
         nxs.erase(nxs.begin() + i);
-        result -= matrix[y][xs[i]] * determinant(matrix, y + 1, nxs);
+        result -= m[y][xs[i]] * determinant(m, y + 1, nxs);
     }
+    
     return result;
 }
 
-void do_slae_with_kramer(const double* const* matrix, int v_num, double* result) {
-    std::vector<int> xs = std::vector<int>(v_num);
-    for (int i = 0; i < v_num; i++) xs[i] = i;
-    double det = determinant(matrix, 0, xs);
+void do_slae_with_kramer(const matrix m, vector& result) {
+    std::vector<int> xs(m.size());
+    for (int i = 0; i < m.size(); i++) xs[i] = i;
+    double det = determinant(m, 0, xs);
     
-    for (int i = 0; i < v_num; i++) {
-        xs[i] = v_num;
-        result[i] = determinant(matrix, 0, xs) / det;
+    for (int i = 0; i < m.size(); i++) {
+        xs[i] = m.size();
+        result[i] = determinant(m, 0, xs) / det;
         xs[i] = i;
     }
 }
@@ -74,23 +76,13 @@ void do_slae_with_kramer(const double* const* matrix, int v_num, double* result)
 // Main
 
 int main() {
-    double *ex = new double[4], *x = new double[4];
-    for (int i = 0; i < 4; i++) ex[i] = i;
-    double **matrix = new_slae_matrix(4, ex);
+    vector ex{0, 1, 2, 3}, x(4);
+    matrix m = Matrix(ex);
     
-    print_matrix(matrix, 4, 5);
-    print_matrix(&ex, 1, 4);
-    std::printf("%6.2f\n", determinant(matrix, 0, {0, 1, 2, 3}));
-    
-    do_slae_with_kramer(matrix, 4, x);
-    
-    std::printf("\n");
-    print_matrix(matrix, 4, 5);
-    print_matrix(&x, 1, 4);
-    
-    delete[] ex;
-    delete[] x;
-    delete_slae_matrix(matrix, 4);
+    std::cout << m << ex;
+    do_slae_with_kramer(m, x);
+    std::cout << '\n' << m << x;
     
     return 0;
 }
+
