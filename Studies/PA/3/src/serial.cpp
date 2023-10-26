0bae52b1b85ef234a5b8a1111423cf0c3c94b38b
dyukoffv4 <net998art@gmail.com>
2023-10-26 16:51:30 +0300

    Speed up from O(n! / 2) to O(n^3) but Segmentation fault

--- Studies/PA/3/src/serial.cpp
+++ Studies/PA/3/src/serial.cpp
@@ -2,46 +2,71 @@
 #include <omp.h>
 
 
-double determinant(const matrix m, int y, std::vector<int> xs) {
-    if (xs.size() == 2) return m[y][xs[0]] * m[y + 1][xs[1]] - m[y][xs[1]] * m[y + 1][xs[0]];
-    double result = 0.0;
-    std::vector<int> nxs;
+void swap(matrix &m, int s1, int s2) {
+    vector temp = m[s1];
+    m[s1] = m[s2];
+    m[s1] = temp;
+}
 
-    for (int i = 0; i < xs.size(); i += 2) {
-        nxs = xs;
-        nxs.erase(nxs.begin() + i);
-        result += m[y][xs[i]] * determinant(m, y + 1, nxs);
+double determinant(const matrix &m) {
+    int m_size = m.size(), e_size = m.size() + 1;
+
+    matrix nm(e_size, vector(e_size, 1));
+    for (int i = 0; i < m_size; i++) for (int j = 0; j < m_size; j++) nm[i + 1][j + 1] = m[i][j];
+
+    for (int k = 1; k < e_size - 1; k++) {
+        if (!nm[k - 1][k - 1]) {
+            for (int i = k; i < e_size; i++) {
+                if (nm[i][k - 1]) {
+                    swap(nm, i, k - 1);
+                    break;
+                }
+            }
+            if (!nm[k - 1][k - 1]) return 0;
+        }
+
+        for (int i = k + 1; i < e_size; i++) {
+            for (int j = k + 1; j < e_size; j++) {
+                nm[i][j] = (nm[i][j] * nm[k][k] - nm[i][k] * nm[k][j]) / nm[k - 1][k - 1];
+            }
+        }
     }
 
-    for (int i = 1; i < xs.size(); i += 2) {
-        nxs = xs;
-        nxs.erase(nxs.begin() + i);
-        result -= m[y][xs[i]] * determinant(m, y + 1, nxs);
-    }
-
-    return result;
+    return nm[m_size][m_size];
 }
 
 void kramerSLAE(const matrix &m, vector &result) {
-    std::vector<int> xs(m.size());
-    for (int i = 0; i < m.size(); i++) xs[i] = i;
-    double det = determinant(m, 0, xs);
+    matrix m2(m.size(), vector(m.size()));
+    for (int i = 0; i < m.size(); i++) for (int j = 0; j < m.size(); j++) m2[i][j] = m[i][j];
 
+    vector b(m.size());
+    for (int i = 0; i < m.size(); i++) b[i] = m[m.size()][i];
+
+    double det = determinant(m2);
+
+    matrix m2_t = m2;
     for (int i = 0; i < m.size(); i++) {
-        xs[i] = m.size();
-        result[i] = determinant(m, 0, xs) / det;
-        xs[i] = i;
+        for (int j = 0; j < m.size(); j++) m2_t[i][j] = b[j];
+        result[i] = determinant(m2_t) / det;
+        for (int j = 0; j < m.size(); j++) m2_t[i][j] = m2[i][j];
     }
 }
 
 
 int main() {
-    vector ex{0, 1, 2, 3}, x(4);
-    matrix m = matrixSLAE(ex);
+    vector ex, x;
+    matrix m;
 
-    std::cout << m << ex;
-    kramerSLAE(m, x);
-    std::cout << '\n' << m << x;
+    for (int i = 5; i < 11; i++) {
+        ex = x = vector(i);
+        for (int j = 0; j < i; j++) ex[j] = j;
+        m = matrixSLAE(ex);
+
+        std::cout << "\nsize: " << i << '\n' << m << '\n' << ex;
+        double t_start = omp_get_wtime();
+        kramerSLAE(m, x);
+        std::cout << "\ntime: " << (omp_get_wtime() - t_start) * 1000 << '\n' << x << '\n';
+    }
 
     return 0;
 }
