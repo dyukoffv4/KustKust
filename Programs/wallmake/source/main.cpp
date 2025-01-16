984f8a5d8152ba5f0cfcbec8f30ca978be986ada
dyukoffv4 <net998art@gmail.com>
2025-01-16 23:52:55 +0300

    Test changes

--- Programs/wallmake/source/main.cpp
+++ Programs/wallmake/source/main.cpp
@@ -2,26 +2,33 @@
 #include <iostream>
 
 
-class HelloWorld : public Gtk::Window {
+class AddButton : public Gtk::Button {
 public:
-    HelloWorld() : m_button("Hello World") {
-        m_button.set_margin(10);
-        m_button.signal_clicked().connect(sigc::mem_fun(*this, &HelloWorld::on_button_clicked));
+    AddButton() {
+        set_margin(10);
+        set_label("+");
+    }
+};
+
+class MainWindow : public Gtk::Window {
+public:
+    MainWindow() {
+        set_title("My App");
+        set_size_request(300, 300);
+        m_button.signal_clicked().connect( sigc::mem_fun(*this, &MainWindow::on_button_clicked) );
         set_child(m_button);
     }
 
-    ~HelloWorld() override {}
-
 protected:
     void on_button_clicked() {
-        std::cout << "Hello World" << std::endl;
+        std::cout << "Hello World!" << std::endl;
     }
 
-    Gtk::Button m_button;
+    AddButton m_button;
 };
 
 
 int main(int argc, char* argv[]) {
     auto app = Gtk::Application::create("org.gtkmm.example");
-    return app->make_window_and_run<HelloWorld>(argc, argv);
+    return app->make_window_and_run<MainWindow>(argc, argv);
 }
