d18d50058cb25fbf8f33b555d87a87ccfda028ea
dyukoffv4 <net998art@gmail.com>
2025-01-17 22:01:11 +0300

    Sudoku solver

--- Programs/wallmake/source/main.cpp
+++ Programs/wallmake/source/main.cpp
@@ -1,30 +1,62 @@
 #include <gtkmm.h>
+#include <vector>
+#include <string>
+#include <thread>
 #include <iostream>
 
 
-class AddButton : public Gtk::Button {
+class NineGroup : public Gtk::Grid {
 public:
-    AddButton() {
-        set_margin(10);
-        set_label("+");
+    NineGroup() : buttons(9) {
+        for (int i = 0; i < 9; i++) {
+            buttons[i].set_label(std::to_string(i + 1));
+            buttons[i].set_margin(5);
+            buttons[i].set_size_request(50, 50);
+            attach(buttons[i], i % 3, i / 3);
+        }
+        set_margin(5);
     }
+
+    std::vector<Gtk::Button> buttons;
 };
 
 class MainWindow : public Gtk::Window {
 public:
-    MainWindow() {
-        set_title("My App");
-        set_size_request(300, 300);
-        m_button.signal_clicked().connect( sigc::mem_fun(*this, &MainWindow::on_button_clicked) );
-        set_child(m_button);
+    MainWindow() : nine_groups(9) {
+        set_title("Sudoku Solver");
+
+        solve_button.set_margin(10);
+        solve_button.set_margin_bottom(20);
+        solve_button.set_label("Solve");
+        solve_button.signal_clicked().connect( sigc::mem_fun(*this, &MainWindow::on_button_clicked) );
+
+        dispatcher.connect( sigc::mem_fun(*this, &MainWindow::on_task_finished) );
+        
+        for (int i = 0; i < 9; i++) main_grid.attach(nine_groups[i], i % 3, i / 3);
+        main_grid.attach(solve_button, 0, 3, 3);
+        set_child(main_grid);
     }
 
 protected:
     void on_button_clicked() {
-        std::cout << "Hello World!" << std::endl;
+        solve_button.set_sensitive(false);
+
+        std::thread([this]() {
+            int sum = 0;
+            for (int i = 0; i < 100000; i++) for (int j = 0; j < 100000; j++) sum++;
+            std::cout << sum << std::endl;
+            dispatcher.emit();
+        }).detach();
     }
 
-    AddButton m_button;
+    void on_task_finished() {
+        solve_button.set_sensitive(true);
+    }
+
+    std::vector<NineGroup> nine_groups;
+    Gtk::Button solve_button;
+    Gtk::Grid main_grid;
+    Glib::Dispatcher dispatcher;
 };
 
 
