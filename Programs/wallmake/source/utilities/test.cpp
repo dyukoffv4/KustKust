1881eb58d37f896346d3d1d042c5b539f5496009
dyukoffv4 <net998art@gmail.com>
2025-01-19 12:28:55 +0300

    Sudoku solve first gen

--- Programs/wallmake/source/utilities/test.cpp
+++ Programs/wallmake/source/utilities/test.cpp
@@ -2,59 +2,120 @@
 #include <iostream>
 
 
-Sudoku::Sudoku(const map& data) : data(data) {
-    rule1 = rule2 = rule3 = pmap(9);
-    for (int i = 0; i < 9; i++) {
-        for (int j = 0; j < 9; j++) {
-            rule1[i].push_back(&(this->data[i][j]));
-            rule2[i].push_back(&(this->data[j][i]));
-            rule3[i / 3 * 3 + j / 3].push_back(&(this->data[i][j]));
-        }
-    }
+Sudoku::Sudoku() {
+    needed_q = needed_c = needed_r = setmap(9, {1, 2, 3, 4, 5, 6, 7, 8, 9});
+    ddata = map(9);
+    for (auto &i : ddata) i = row(9, 0);
 }
 
-bool Sudoku::check_health() {
-    std::set<int> container;
-    for (auto &c : std::vector<pmap>{rule1, rule2, rule3}) {
-        for (auto &i : c) {
-            for (auto &j : i) {
-                if (*j != 0) {
-                    if (container.count(*j)) return false;
-                    container.insert(*j);
-                }
+bool Sudoku::load(const map& data) {
+    if (data.size() != 9) return false;
+    for (auto &i : data) if (i.size() != 9) return false;
+
+    std::set<int> *set_r, *set_c, *set_q;
+    int tile;
+
+    for (int i = 0; i < 9; i++) {
+        for (int j = 0; j < 9; j++) {
+            tile = data.at(i).at(j);
+
+            if (tile < 0 || tile > 9) {
+                needed_q = needed_c = needed_r = setmap(9, {1, 2, 3, 4, 5, 6, 7, 8, 9});
+                return false;
             }
-            container.clear();
+
+            if (tile = data.at(i).at(j)) {
+                set_r = &needed_r[i];
+                set_c = &needed_c[j];
+                set_q = &needed_q[i / 3 * 3 + j / 3];
+                
+                if (!set_r->count(tile) || !set_c->count(tile) || !set_q->count(tile)) {
+                    needed_q = needed_c = needed_r = setmap(9, {1, 2, 3, 4, 5, 6, 7, 8, 9});
+                    return false;
+                }
+                
+                set_r->erase(tile);
+                set_c->erase(tile);
+                set_q->erase(tile);
+            }
         }
     }
+
+    ddata = data;
+
     return true;
 }
 
+Sudoku::map Sudoku::data() {
+    return ddata;
+}
+
 bool Sudoku::solve() {
-    // Not done yet
+    std::set<int> rows, cols;
+    int irow, icol, open;
+
+    for (int tile = 0; tile < 9; tile++) {
+        for (auto &i : needed_q[tile]) {
+            for (int r = tile / 3; r < tile / 3 + 2; r++) {
+                if (needed_r[r].count(i)) rows.insert(r);
+            }
+            for (int c = tile % 3 * 3; c < tile % 3 * 3 + 2; c++) {
+                if (needed_c[c].count(i)) cols.insert(c);
+            }
+
+            open = rows.size() * cols.size();
+            for (auto &r : rows) {
+                for (auto &c : cols) {
+                    if (ddata[r][c]) open--;
+                    else {
+                        irow = r;
+                        icol = c;
+                    }
+                }
+            }
+
+            rows.clear();
+            cols.clear();
+
+            if (open == 0) return false;
+            if (open == 1) {
+                ddata[irow][icol] = i;
+                needed_r[irow].erase(i);
+                needed_c[icol].erase(i);
+                needed_q[tile].erase(i);
+                tile = -1;
+                break;
+            }
+        }
+    }
+
     return true;
 }
 
 bool Sudoku::solve(map& table) {
-    // Not done yet
+    if (!solve()) return false;
+    table = ddata;
     return true;
 }
 
-Sudoku::map Sudoku::get_data() {
-    return data;
-}
 
-/*
 int main(int argc, char* argv[]) {
     std::cout << "Enter sudoku template. Print zero in empty places.\n\n";
 
     Sudoku::map table(9, Sudoku::row(9));
     for (auto& i : table) for (auto& j : i) { std::cin >> j; if (j < 0 || j > 9) return 1; }
 
-    Sudoku sudoku(table);
-    if (!sudoku.check_health()) return 2;
+    Sudoku sudoku;
+    if (!sudoku.load(table)) return 2;
     
     std::cout << "\nStart calculating.\n\n";
+    if (!sudoku.solve(table)) return 3;
+
+    std::cout << "Result.\n\n";
+    for (auto& i : table) {
+        for (auto& j : i) std::cout << j << ' ';
+        std::cout << '\n';
+    }
 
     return 0;
 }
-*/
--- Programs/wallmake/source/utilities/test.hpp
+++ Programs/wallmake/source/utilities/test.hpp
@@ -6,22 +6,19 @@ class Sudoku {
 public:
     typedef std::vector<int> row;
     typedef std::vector<row> map;
+    typedef std::vector<std::set<int>> setmap;
 
-    typedef std::vector<int*> prow;
-    typedef std::vector<prow> pmap;
+    Sudoku();
 
-    Sudoku(const map& data);
-
-    bool check_health();
+    bool load(const map& data);
+    map data();
 
     bool solve();
     bool solve(map& table);
 
-    map get_data();
-
 private:
-    map data;
-    pmap rule1;
-    pmap rule2;
-    pmap rule3;
+    map ddata;
+    setmap needed_q;
+    setmap needed_r;
+    setmap needed_c;
 };
